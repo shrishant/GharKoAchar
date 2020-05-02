ec2982d03abf6c7c964b828b0dc58ffcc0df2057
unknown <Shrishant>
2020-05-02 23:20:10 +0545

    upper header file finished

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Router as BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import '../assets/css/Main.style.css';
 import history from 'utils/history';
+
+import TopHeader from  './topheader/topHeader.components'
 
 import * as routes from 'constants/routes';
 
-import Home from './home';
-import Login from './login';
 
 import PrivateRoute from './common/routes/PrivateRoute';
 
 // Top level application router.
 const Router = () => (
   <BrowserRouter history={history}>
+    <TopHeader/>
     <Switch>
-      <Route exact path={routes.LOGIN} component={Login} />
-      <PrivateRoute path={routes.HOME} component={Home} />
     </Switch>
   </BrowserRouter>
 );
